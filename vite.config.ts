66a263eae0b11982cda400b53083697dcaec3b4f
Pack Aussadawut <75838193+PackHubTH@users.noreply.github.com>
2023-11-03 13:50:35 +0700

    Initialize twin-macro

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import EnvironmentPlugin from 'vite-plugin-environment'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
+  esbuild: {
+    // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+      },
+    }),
+    tsconfigPaths(),
+    EnvironmentPlugin('all', {
+      prefix: 'VITE_',
+    }),
+  ],
+})
