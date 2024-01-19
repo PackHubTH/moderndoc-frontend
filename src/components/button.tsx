e2f6f309b72b84b787d0d0c3fbced057ed336366
DylDarab <apicha.oap@gmail.com>
2024-01-19 14:01:49 +0700

    `fix`: prettier not working

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 type PropsType = {
   children: React.ReactNode
+  color?: string
+  backgroundColor?: string
   onClick: () => void
 }
-const Button: React.FC<PropsType> = ({ children, onClick }) => {
+const Button: React.FC<PropsType> = ({
+  children,
+  onClick,
+  color = 'white',
+  backgroundColor,
+}) => {
   return (
     <button className="bg-red-500" onClick={onClick}>
       {children}
