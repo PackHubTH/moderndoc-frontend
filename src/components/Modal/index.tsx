6caf81f1991d7861ca14140bd67f1c619a4fadf2
DylDarab <apicha.oap@gmail.com>
2024-03-05 02:56:47 +0700

    `feat`: create faq 75%

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -12,6 +12,7 @@ type PropsType = {
   actions?: React.ReactNode
   leftIcon?: React.ReactNode
   variant?: VariantType
+  width?: string
 }
 
 const Modal: React.FC<PropsType> = ({
@@ -21,6 +22,7 @@ const Modal: React.FC<PropsType> = ({
   onClose,
   actions,
   leftIcon,
+  width,
   variant = 'default',
 }) => {
   return (
@@ -49,7 +51,12 @@ const Modal: React.FC<PropsType> = ({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="relative w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel
+                css={[
+                  tw`relative w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all`,
+                  width && { width, maxWidth: width },
+                ]}
+              >
                 <IoClose
                   size="24px"
                   className="absolute right-3 top-4 cursor-pointer rounded-full border border-transparent text-gray-500 hover:bg-gray-100"
--- src/modules/faq/pages/FaqManagementPage/CreateFaqModal/index.tsx
+++ src/modules/faq/pages/FaqManagementPage/CreateFaqModal/index.tsx
@@ -1,8 +1,11 @@
 import Button from '@/components/Button'
+import JsonTextInput from '@/components/JsonTextInput'
 import Modal from '@/components/Modal'
+import RadioGroup from '@/components/RadioGroup'
 import RichTextInput from '@/components/RichTextInput'
 import TextInput from '@/components/TextInput'
 import useCreateFaqForm from '@/modules/hooks/useCreateFaqForm'
+import { SendChannel } from '@/modules/types'
 import { Controller } from 'react-hook-form'
 import { FaPlus } from 'react-icons/fa6'
 
@@ -13,8 +16,11 @@ type PropsType = {
 const CreateFaqModal: React.FC<PropsType> = ({ isOpen, onClose }) => {
   const { methods } = useCreateFaqForm()
 
+  console.log('methods', methods.watch())
+
   return (
     <Modal
+      width="800px"
       title={
         <span className="mt-4 flex items-center justify-center gap-2">
           <FaPlus
@@ -25,7 +31,7 @@ const CreateFaqModal: React.FC<PropsType> = ({ isOpen, onClose }) => {
         </span>
       }
       content={
-        <form className="space-y-5">
+        <form className="max-h-[400px] space-y-5 overflow-y-auto p-1">
           <Controller
             control={methods.control}
             name="titleTh"
@@ -74,14 +80,65 @@ const CreateFaqModal: React.FC<PropsType> = ({ isOpen, onClose }) => {
             control={methods.control}
             name="description"
             render={({ field }) => (
-              //   <TextInput
-              //     label="รายละเอียด"
-              //     placeholder="กรอกรายละเอีนด"
-              //     {...field}
-              //   />
               <RichTextInput
                 label="รายละเอียด"
                 placeholder="กรอกรายละเอียด"
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            control={methods.control}
+            name="sendChannel"
+            render={({ field }) => (
+              <div>
+                <RadioGroup
+                  label="แนวทางการส่งเอกสาร"
+                  options={[
+                    {
+                      label: 'สามารถส่งเอกสารภายในระบบได้ (Online)',
+                      value: SendChannel.ONLINE,
+                    },
+                    {
+                      label: 'ไม่สามารถส่งเอกสารภายในระบบได้ (Onsite)',
+                      value: SendChannel.ONSITE,
+                    },
+                    {
+                      label: 'สามารถส่งได้ทั้ง 2 รูปแบบ',
+                      value: SendChannel.BOTH,
+                    },
+                    { label: 'อื่นๆ', value: SendChannel.OTHER },
+                  ]}
+                  {...field}
+                />
+                <Controller
+                  control={methods.control}
+                  name="sendChannelInfo"
+                  render={({ field }) => (
+                    <TextInput
+                      placeholder="กรอกอีเมล/ชื่อ หรือแนวทางการส่งเอกสาร"
+                      {...field}
+                    />
+                  )}
+                />
+              </div>
+            )}
+          />
+          <Controller
+            control={methods.control}
+            name="extraContact"
+            render={({ field }) => (
+              <JsonTextInput
+                label="ช่องทางการติดต่อเพิ่มเติม (ถ้ามี)"
+                fields={[
+                  'ที่ตั้ง',
+                  'วันเวลาทำการ',
+                  'เบอร์โทรศัพท์',
+                  'เว็ปไซต์ (Link URL)',
+                  'เพจ Facebook',
+                  'Line ID',
+                  'อื่นๆ',
+                ]}
                 {...field}
               />
             )}
